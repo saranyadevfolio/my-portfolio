99e35cb521e4fddf46a19f46573a787edb7cb8a3
saranya-2025 <Saranya@users.noreply.github.com>
2026-05-24 09:41:09 +0530

    removed pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>   
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>My Portfolio</h1>
+    </div>
   );
 }
 
